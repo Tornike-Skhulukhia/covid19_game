226729d03fc02a7b66f8cc21f7578551bd11033c
Tornike <tornike.skhulukhia.1@iliauni.edu.ge>
2020-04-22 22:47:42 +0400

    some changes...

--- stop_covid/stop_covid19_game.py
+++ stop_covid/stop_covid19_game.py
@@ -2,9 +2,11 @@
 import pygame
 import random
 import time
+import numpy as np
+import re
 
 # define things
-WIDTH = 720
+WIDTH = 320
 HEIGHT = 480
 SCREEN_SIZE = (WIDTH, HEIGHT)
 BG_COLOR = (20, 20, 20)
@@ -12,12 +14,12 @@ BG_COLOR = (20, 20, 20)
 # initialize
 pygame.display.init()
 
-pygame.mixer.pre_init(44100, -16, 1, 512)
-pygame.mixer.init()
+# pygame.mixer.pre_init(44100, -16, 1, 512)
+# pygame.mixer.init()
 
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
-WALL_HIT_SOUND = pygame.mixer.Sound('audio/basic_fist_hit.wav')
+# WALL_HIT_SOUND = pygame.mixer.Sound('audio/basic_fist_hit.wav')
 
 # ICON = pygame.image.load('enemy_100px.png')
 # pygame.display.set_icon(ICON)
@@ -33,11 +35,13 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self, screen, img='img/enemy_48.png', speed_x=2, speed_y=2, x=0, y=0, groups=[]):
         super().__init__(*groups)
         self.screen = screen
+        # self.img = pygame.image.load(img)
         self.img = pygame.image.load(img)
         self.rect = self.img.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.mask = pygame.mask.from_surface(self.img)
+        self.mass = 4/3 * np.pi * (int(re.search(r'(\d{1,3})\.png',img).group(1)) // 2 )**3
 
         self.speed_x = speed_x
         self.speed_y = speed_y
@@ -45,13 +49,28 @@ class Enemy(pygame.sprite.Sprite):
         self.update_y_width = self.rect.height + abs(self.speed_y)
 
 
-    def increase_speed_by(self, x_num, y_num):
-        self.speed_x += x_num
-        self.speed_y += y_num
+
+    def get_mass(self):
+        return self.mass
+
+
+    def get_speed(self):
+        return (self.speed_x, self.speed_y)
+
+
+    def set_speed(self, x_num, y_num):
+        self.speed_x = x_num
+        self.speed_y = y_num
 
         # update rectangles for updates
         self.update_x_width = self.rect.width + abs(self.speed_x)
         self.update_y_width = self.rect.height + abs(self.speed_y)
+
+
+    def increase_speed_by(self, delta_x, delta_y):
+        x, y = self.get_speed()
+        self.set_speed(x + delta_x, y + delta_y)
+
 
     def draw(self):
         self.screen.blit(self.img, self.rect)
@@ -118,11 +137,15 @@ class Enemy(pygame.sprite.Sprite):
         return update_region
 
 
+
 # def main():        
-enemies_num = 4
+enemies_num = 5
 enemy_pics = [
+                # 'img/enemy_16.png',
+                # 'img/enemy_32.png',
                 'img/enemy_48.png',
                 # 'img/enemy_64.png',
+                # 'img/enemy_84.png',
                 # 'img/enemy_84.png',
                 # 'img/enemy_84.png',
             ] * enemies_num
@@ -133,6 +156,9 @@ ALL_SPRITES = pygame.sprite.Group()
 enemies = [
             Enemy(
                 screen=screen,
+                # x=random.choice(range((WIDTH // enemies_num) *i, (WIDTH // enemies_num)  * (i+1))),
+                # y=random.choice(range((HEIGHT // enemies_num) *i, (HEIGHT // enemies_num)  * (i+1))),
+
                 x=random.choice(range(WIDTH)),
                 y=random.choice(range(HEIGHT)),
                 speed_x=random.randint(1, 3),
@@ -150,7 +176,6 @@ screen.fill(BG_COLOR)
 pygame.display.update()
 
 
-
 FPS = 30
 
 run = 1
@@ -161,29 +186,28 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: run = 0
 
+    screen.fill(BG_COLOR)
+    # pygame.display.update()
+
     # redraw enemies
     for enemy in enemies:
-        screen.fill(BG_COLOR, enemy.rect)
+        # screen.fill(BG_COLOR, enemy.rect)
         update_region = enemy.move()
         
-        pygame.display.update(update_region)
+        # pygame.display.update(update_region)
 
         # collisions
         ALL_SPRITES.remove(enemy)
         collided = pygame.sprite.spritecollide(enemy, ALL_SPRITES, False, pygame.sprite.collide_mask)
 
         if collided:
-            # [enemy.reverse_y_speed,enemy.reverse_x_speed][random.choice([0, 1])]()
             enemy.reverse_y_speed()
             enemy.reverse_x_speed()
-            
-            enemy.increase_speed_by(1, 1)
-            print('speed increased by 1x1')
-            # breakpoint()
 
         ALL_SPRITES.add(enemy)
 
 
+    pygame.display.update()
     # redraw player
 
 pygame.quit()
